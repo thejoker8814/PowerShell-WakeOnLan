d2bb0f33e75c1aa80508ce49605457032e4858bd
theJoker8814 <felix.buehl@febit.systems>
2022-03-25 05:45:04 +0100

    add link (repository renamed)

--- Invoke-WakeOnLan.ps1
+++ Invoke-WakeOnLan.ps1
@@ -26,7 +26,7 @@ function Invoke-WakeOnLan
     Author: felix.buehl@febit.systems
     Filename: Invoke-WakeOnLan.ps1
 .LINK
-   https://github.com/thejoker8814/PowerShellScripts/blob/main/Invoke-WakeOnLan.ps1
+    https://github.com/thejoker8814/PowerShell-WakeOnLan
 #>
   [CmdletBinding()]
   param
